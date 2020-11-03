8e7bb2551d699d176f7840dd15a050ddf507dbdc
MrZHLF <480970024@qq.com>
2020-11-03 09:02:10 +0800

    '初始化页面'

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container">
+    <global-header :user="user"></global-header>
+    <column-list :list="list"></column-list>
   </div>
-  <router-view/>
 </template>
-
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, {ColumnProps} from './components/ColumnList.vue'
+import GlobalHeader, {UserProps} from './components/GlobalHeader.vue'
+const currentUser: UserProps = {
+  isLogin: false
+}
+const TestData: ColumnProps[]= [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧, 这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_150,w_150'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧 这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  }
+]
+export default defineComponent({
+  components: {
+    ColumnList,
+    GlobalHeader
+  },
+  setup () {
+    
+    return {
+      list: TestData,
+      user:currentUser
+    }
+  }
+})
+</script>
 <style lang="scss">
-#app {
+html,body,#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    <a href="#" class="navbar-brand">者也专栏</a>
+    <ul class="list-inline mb-0" v-if="!user.isLogin">
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">登陆</a></li>
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
+    </ul>
+    <ul v-else class="list-inline mb-0">
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">你好！小米</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number;
+}
+export default defineComponent({
+  props:{
+    user:{
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
